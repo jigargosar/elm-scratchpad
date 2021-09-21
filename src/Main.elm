6645cb28512f91b01653e64aeefeff02915faa08
jigargosar <jigar.gosar@gmail.com>
2021-09-21 14:39:29 +0530

    added some svg

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,16 @@
 module Main exposing (main)
 
 import Html exposing (text)
+import Html.Attributes exposing (style)
+import Svg
+import TypedSvg.Attributes.InPx as Px
 
 
 main =
-    Svg.svg [] [ text "hi" ]
+    Svg.svg [ style "font-size" "20px" ]
+        [ Svg.text_
+            [ Px.x 10
+            , Px.y 10
+            ]
+            [ text "hi" ]
+        ]
