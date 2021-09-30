fbf9119c8fc308d5f9d71b00cbd0a0219d1eaf50
jigargosar <jigar.gosar@gmail.com>
2021-09-30 15:13:35 +0530

    adjusting to match p5 example

--- src/Main.elm
+++ src/Main.elm
@@ -13,13 +13,70 @@ wc =
     { w = 710, h = 400 }
 
 
+type alias Model =
+    { current : ( Float, Float )
+    , history : List ( Point, Point )
+    , seed : Random.Seed
+    }
+
+
+initialModel : Model
+initialModel =
+    { current = ( wc.w / 2, wc.h / 2 )
+    , history = []
+    , seed = Random.initialSeed 0
+    }
+
+
+makeMove : Model -> Model
+makeMove model =
+    let
+        motionGenerator =
+            Random.float -6 6
+
+        ( ( dx, dy ), seed ) =
+            Random.step
+                (Random.pair motionGenerator motionGenerator)
+                model.seed
+
+        ( x, y ) =
+            model.current
+
+        next =
+            ( clamp 0 wc.w (x + dx)
+            , clamp 0 wc.h (y + dy)
+            )
+    in
+    { model
+        | current = next
+        , seed = seed
+        , history = ( next, model.current ) :: model.history
+    }
+
+
+makeNMoves : Int -> Model -> Model
+makeNMoves n model =
+    if n <= 0 then
+        model
+
+    else
+        makeNMoves (n - 1) (makeMove model)
+
+
 main =
+    let
+        model =
+            initialModel
+                |> makeNMoves 2000
+    in
     Svg.svg
         [ style "font-size" "20px"
+        , style "background-color" "#333"
         , SA.width <| String.fromFloat wc.w
         , SA.height <| String.fromFloat wc.h
         ]
-        [ Svg.g [ TA.transform [ TT.Translate 50 50 ] ]
+        [ viewMotion model
+        , Svg.g [ TA.transform [ TT.Translate 50 50 ] ]
             [ drawWalk 0 "blue"
             , drawWalk 1 "green"
             , drawWalk 2 "red"
@@ -27,6 +84,11 @@ main =
             , Svg.circle [ Px.r 5 ] []
             ]
         ]
+
+
+viewMotion model =
+    List.indexedMap (\i ( a, b ) -> drawLine_V2 a b 0.5) model.history
+        |> Svg.g [ SA.stroke "white" ]
 
 
 drawWalk i c =
