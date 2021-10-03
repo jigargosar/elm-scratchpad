a1374b4147d05a3b6487c859048d17a6edfa2ec4
jigargosar <jigar.gosar@gmail.com>
2021-10-03 12:34:17 +0530

    model and mock snake

--- src/SnakeGame.elm
+++ src/SnakeGame.elm
@@ -6,10 +6,8 @@ import Html.Attributes exposing (style)
 import Random exposing (Generator, Seed)
 import Svg exposing (Svg)
 import Svg.Attributes as SA
-import Svg.Events as SE
 import Time
 import TypedSvg.Attributes as TA
-import TypedSvg.Types as TT
 
 
 main : Program () Model Msg
@@ -43,12 +41,12 @@ type alias Seg =
 
 
 type alias Model =
-    {}
+    { seed : Seed }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( {}
+    ( { seed = Random.initialSeed 0 }
     , Cmd.none
     )
 
@@ -65,7 +63,7 @@ update msg model =
 
 
 view : Model -> Html Msg
-view model =
+view _ =
     Svg.svg
         [ style "font-size" "20px"
         , style "background-color" "#333"
@@ -81,10 +79,87 @@ view model =
         ]
 
 
+snakePoints : List Point
+snakePoints =
+    --List.range 5 10
+    --    |> List.map (\ix -> ( toFloat ix * cellSize, 10 * cellSize ))
+    initialSnake
+        |> applyN 10 moveAndExtendSnake
+        |> applyN 10 moveSnake
+        |> snakeToPoints
+
+
+applyN n fn x =
+    if n <= 0 then
+        x
+
+    else
+        applyN (n - 1) fn (fn x)
+
+
+type alias GridPoint =
+    ( Int, Int )
+
+
+type alias Snake =
+    { head : GridPoint
+    , tail : List GridPoint
+    , direction : Direction
+    }
+
+
+type Direction
+    = Right
+
+
+initialSnake : Snake
+initialSnake =
+    { head = ( 10, 10 )
+    , tail = []
+    , direction = Right
+    }
+
+
+moveAndExtendSnake : Snake -> Snake
+moveAndExtendSnake snake =
+    let
+        ( hx, hy ) =
+            snake.head
+
+        nextHead =
+            ( hx + 1, hy )
+    in
+    { snake | head = nextHead, tail = snake.head :: snake.tail }
+
+
+moveSnake : Snake -> Snake
+moveSnake snake =
+    let
+        ( hx, hy ) =
+            snake.head
+
+        nextHead =
+            ( hx + 1, hy )
+    in
+    { snake | head = nextHead, tail = snake.head :: snake.tail |> dropLast }
+
+
+dropLast : List a -> List a
+dropLast =
+    List.reverse >> List.drop 1 >> List.reverse
+
+
+snakeToPoints : Snake -> List Point
+snakeToPoints snake =
+    snake.head
+        :: snake.tail
+        |> List.map gridPointToPoint
+
+
+gridPointToPoint : GridPoint -> Point
+gridPointToPoint ( x, y ) =
+    ( toFloat x * cellSize, toFloat y * cellSize )
+
+
 cellSize =
     10
-
-
-snakePoints =
-    List.range 5 10
-        |> List.map (\ix -> ( toFloat ix * cellSize, 10 * cellSize ))
