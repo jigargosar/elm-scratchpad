37b6c59f2d3db82b77aeba983b99fdb5887647fe
jigargosar <jigar.gosar@gmail.com>
2021-11-13 13:42:39 +0530

    SlideCircleGame

---
+++ src/SlideCircleGame.elm
@@ -0,0 +1,203 @@
+module SlideCircleGame exposing (..)
+
+import Browser
+import Dict exposing (Dict)
+import Html exposing (Attribute, Html)
+import Svg exposing (Svg)
+import Svg.Attributes as SA
+import Svg.Events as SE
+import Time
+import TypedSvg.Attributes as TA
+import TypedSvg.Attributes.InPx as Px
+import Utils exposing (..)
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+width =
+    gw * cz
+
+
+height =
+    gh * cz
+
+
+gw =
+    3
+
+
+gh =
+    4
+
+
+cz =
+    160
+
+
+gpToWorld : GPos -> Vec
+gpToWorld =
+    vFromGP >> vScale cz >> vAdd1 (cz / 2)
+
+
+type alias Model =
+    { tiles : TilesDict
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( { tiles = initialTiles }
+    , Cmd.none
+    )
+
+
+type Msg
+    = OnTick
+    | GPClicked GPos
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Time.every (1000 / 60) (\_ -> OnTick)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        OnTick ->
+            ( model, Cmd.none )
+
+        GPClicked gp ->
+            ( onGPClick gp model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    Svg.svg
+        [ saWidth width
+        , saHeight height
+        , noFill
+        , noStroke
+        , bgc gray
+        , noUserSelect
+        ]
+        [ model.tiles
+            |> Dict.toList
+            |> List.map viewTile3
+            |> group []
+        ]
+
+
+viewCircles =
+    group
+        [ stroke "aqua"
+        , SA.strokeWidth "20"
+        , SA.opacity "0.7"
+        , xf [ mv2 (width / 2) (height / 2) ]
+        ]
+        [ circle (cz * 1.3) [ xf [ mv2 0 (cz / 2) ] ]
+        , circle (cz * 0.3) [ xf [ mv2 0 (-height * 0.5 + cz * 0.5) ] ]
+        ]
+
+
+circle r xs =
+    Svg.circle (Px.r r :: xs) []
+
+
+type alias Tile =
+    { index : Int, ogp : GPos }
+
+
+type alias TilesDict =
+    Dict GPos Tile
+
+
+initialTiles : TilesDict
+initialTiles =
+    let
+        gps =
+            rangeWH gw gh
+
+        --|> List.take (gw * gh - 1)
+        ins t =
+            Dict.insert t.ogp t
+    in
+    gps
+        |> List.indexedMap Tile
+        |> List.filter (.index >> neq 8)
+        |> List.foldl ins Dict.empty
+
+
+getEmptyGP : TilesDict -> GPos
+getEmptyGP td =
+    case
+        rangeWH gw gh
+            |> List.filter (\k -> Dict.member k td |> not)
+            |> List.head
+    of
+        Nothing ->
+            Debug.todo "this should never happen"
+
+        Just gp ->
+            gp
+
+
+onGPClick : GPos -> Model -> Model
+onGPClick gp model =
+    let
+        updatedTiles =
+            case ( Dict.get gp model.tiles, areAdjacent gp (getEmptyGP model.tiles) ) of
+                ( Just gpTile, True ) ->
+                    model.tiles
+                        |> Dict.remove gp
+                        |> Dict.insert (getEmptyGP model.tiles) gpTile
+
+                _ ->
+                    model.tiles
+    in
+    { model | tiles = updatedTiles }
+
+
+viewTile : ( GPos, Tile ) -> Html Msg
+viewTile ( gp, t ) =
+    group [ xf [ mv (gpToWorld gp) ] ]
+        [ square cz [ fillTransparent ]
+        , words
+            [ fill white
+            , xf [ scale 3 ]
+            ]
+            (viewIndex t)
+        ]
+
+
+viewIndex : Tile -> String
+viewIndex tile =
+    tile.index |> String.fromInt
+
+
+viewTile2 : ( GPos, Tile ) -> Html Msg
+viewTile2 ( ( gx, gy ), t ) =
+    let
+        ( ogx, ogy ) =
+            t.ogp
+    in
+    Svg.svg
+        [ saWidth cz
+        , saHeight cz
+        , TA.viewBox (toFloat ogx * cz) (toFloat ogy * cz) cz cz
+        ]
+        [ viewCircles ]
+        |> List.singleton
+        |> group [ xf [ mv2 (toFloat gx * cz) (toFloat gy * cz) ] ]
+
+
+viewTile3 (( gp, _ ) as x) =
+    group [ SE.onClick (GPClicked gp) ] [ viewTile x, viewTile2 x ]
