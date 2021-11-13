fd1c7a742b00232b7a2aa20b14ce6cbca3a7681f
jigargosar <jigar.gosar@gmail.com>
2021-11-13 15:31:16 +0530

    SlideCircleGame

--- src/SlideCircleGame.elm
+++ src/SlideCircleGame.elm
@@ -179,6 +179,7 @@ viewTileAt ( gp, t ) =
     group
         [ SE.onClick (GPClicked gp)
         , xf [ mv (gpToCenterWC gp) ]
+        , style "transition" "all 1s"
         ]
         [ viewTileFG t
         , viewTileBG t
