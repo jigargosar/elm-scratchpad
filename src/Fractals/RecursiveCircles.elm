9c8c6497f9c2078e0f54c48be470c6319e5cd5b0
jigargosar <jigar.gosar@gmail.com>
2021-11-25 11:24:10 +0530

    solving SlideTileGame.elm v3

---
+++ src/Fractals/RecursiveCircles.elm
@@ -0,0 +1,32 @@
+module Fractals.RecursiveCircles exposing (..)
+
+import Playground exposing (..)
+
+
+main =
+    picture
+        [ genRadii { min = 5, step = 5 } 150 []
+            |> List.map (strokeCircle 2)
+            |> group
+        ]
+
+
+genRadii :
+    { a | min : number, step : number }
+    -> number
+    -> List number
+    -> List number
+genRadii ({ min, step } as config) r ls =
+    if r < min then
+        ls |> List.reverse
+
+    else
+        genRadii config (r - abs step) (r :: ls)
+
+
+strokeCircle : Number -> Number -> Shape
+strokeCircle strokeWidth r =
+    group
+        [ circle black r
+        , circle white (r - strokeWidth)
+        ]
