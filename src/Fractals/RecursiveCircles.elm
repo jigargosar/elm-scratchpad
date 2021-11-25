f7e9c54f39ffa406eb1df29b73b2a4796f9f12b7
jigargosar <jigar.gosar@gmail.com>
2021-11-25 12:36:07 +0530

    Fractals - RecursiveCircles

--- src/Fractals/RecursiveCircles.elm
+++ src/Fractals/RecursiveCircles.elm
@@ -1,79 +1,42 @@
 module Fractals.RecursiveCircles exposing (..)
 
-import Playground exposing (..)
+import Svg
+import Svg.Attributes as SA
+import Utils exposing (..)
+
+
+width =
+    500
+
+
+height =
+    500
 
 
 main =
-    picture
-        [ genRadii { min = 5, step = 10 } 1000 []
-            |> List.map (strokeCircle 2)
-            |> group
-        , radii
-            |> List.reverse
-            |> Debug.log "radii"
-            |> List.map (strokeCircle 2)
-            |> group
-            |> fade 0.5
-        , radii2
-            |> Debug.log "radii"
-            |> List.map (strokeCircle 2)
-            |> group
-            |> fade 0.5
+    Svg.svg
+        [ style "width" "100vw"
+        , style "height" "100vh"
+        , dBlock
+        , noFill
+        , noStroke
+        ]
+        [ group
+            [ style "transform" "translate(50%,50%)"
+            , strokeW 1
+            , stroke black
+            ]
+            [ genCirc 0 0 1000 []
+                |> List.map (\( x, y, r ) -> circle r [ xf [ mv2 x y ] ])
+                |> group []
+            ]
         ]
 
 
-radii2 =
-    iterate
-        (\r ->
-            if r > 1000 then
-                Nothing
-
-            else
-                Just (r * 1.75)
-        )
-        5
-        []
-
-
-radii =
-    iterate
-        (\r ->
-            if r < 5 then
-                Nothing
-
-            else
-                Just (r * 0.75)
-        )
-        1000
-        []
-
-
-iterate : (a -> Maybe a) -> a -> List a -> List a
-iterate mbFn root xs =
-    case mbFn root of
-        Nothing ->
-            xs
-
-        Just nextRoot ->
-            iterate mbFn nextRoot (root :: xs)
-
-
-genRadii :
-    { a | min : number, step : number }
-    -> number
-    -> List number
-    -> List number
-genRadii ({ min, step } as config) r ls =
-    if r < min then
-        ls |> List.reverse
+genCirc : Float -> Float -> Float -> List ( Float, Float, Float ) -> List ( Float, Float, Float )
+genCirc x y r acc =
+    if r > 2 then
+        genCirc x y (r * 0.75) (( x, y, r ) :: acc)
 
     else
-        genRadii config (r - abs step) (r :: ls)
-
-
-strokeCircle : Number -> Number -> Shape
-strokeCircle strokeWidth r =
-    group
-        [ circle black r
-        , circle white (r - strokeWidth)
-        ]
+        ( x, y, r ) :: acc
