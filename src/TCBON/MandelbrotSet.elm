8e2b22a2c0ef326a80af905413bc06cdfd24ce97
jigargosar <jigar.gosar@gmail.com>
2021-11-29 16:21:50 +0530

    MandelbrotSet.elm

--- src/TCBON/MandelbrotSet.elm
+++ src/TCBON/MandelbrotSet.elm
@@ -31,12 +31,23 @@ main =
                             b =
                                 toFloat y |> rangeMap ( 0, 100 ) ( -2, 2 )
                         in
-                        sqrt (a ^ 2 + b ^ 2) < 2
+                        iterate a b 50 0 0
                     )
                 |> List.map (vFromIntTuple >> (\p -> square 1 [ xf [ mv p ] ]))
             )
         ]
 
 
-foo =
-    rangeMap
+iterate cx cy n a b =
+    let
+        isLessThan2 =
+            sqrt (a ^ 2 + b ^ 2) < 2
+    in
+    if n <= 0 then
+        isLessThan2
+
+    else if isLessThan2 then
+        iterate cx cy (n - 1) (cx + a ^ 2) (cy + b ^ 2)
+
+    else
+        False
