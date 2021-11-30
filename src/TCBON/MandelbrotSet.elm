9f6dc771276edf24b7e77a94b69989c29b1849d5
jigargosar <jigar.gosar@gmail.com>
2021-11-30 14:35:35 +0530

    MandelbrotSet.elm

--- src/TCBON/MandelbrotSet.elm
+++ src/TCBON/MandelbrotSet.elm
@@ -44,8 +44,7 @@ initialBounds : Bounds
 initialBounds =
     --{ min = vec -2.4 -1.4, max = vec 1.34 1.4 }
     --{ min = vec -2.2 -1.4, max = vec 0.6 1.4 }
-    boundsFromWH 3 2
-        |> centerBoundsAt -0.8 0
+    boundsFromWH 3 2 |> centerBoundsAt -0.8 0
 
 
 boundsFromWH : Float -> Float -> Bounds
