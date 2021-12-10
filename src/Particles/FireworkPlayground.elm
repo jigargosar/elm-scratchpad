465faf2fa86f2bf9162dc3baf45b9eebd0d94288
jigargosar <jigar.gosar@gmail.com>
2021-12-10 13:53:20 +0530

    Firework Playground

---
+++ src/Particles/FireworkPlayground.elm
@@ -0,0 +1,42 @@
+module Particles.FireworkPlayground exposing (..)
+
+import Playground exposing (..)
+
+
+main =
+    game view update initial
+
+
+type alias Model =
+    {}
+
+
+initial =
+    Model
+
+
+update : Computer -> Model -> Model
+update c m =
+    m
+
+
+view : Computer -> Model -> List Shape
+view c m =
+    let
+        s : Screen
+        s =
+            c.screen
+
+        minV =
+            min s.width s.height
+
+        oneHundredth =
+            minV / 100
+
+        pct n =
+            n * oneHundredth
+    in
+    [ rectangle white s.width s.height
+    , square black minV
+    , circle green (pct 5)
+    ]
