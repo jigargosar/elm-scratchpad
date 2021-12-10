8351a4021de62081fe7e36ca7df09b51ddfdd457
jigargosar <jigar.gosar@gmail.com>
2021-12-10 16:05:44 +0530

    Firework Svg

--- src/Particles/FireworkSvg.elm
+++ src/Particles/FireworkSvg.elm
@@ -1,5 +1,6 @@
 module Particles.FireworkSvg exposing (..)
 
+import Color
 import Utils exposing (..)
 
 
@@ -17,4 +18,8 @@ main =
 trail s e =
     sampleVecFromTo 50 s e
         |> List.map (\p -> circle 5 [ xf [ mv p ] ])
-        |> group [ fill white ]
+        |> group [ fill <| hsl 0.1 0.5 0.5 ]
+
+
+hsl h s l =
+    Color.hsl h s l |> Color.toCssString
