6cc9921ebd893c5132c07258ac86509a84b2b993
jigargosar <jigar.gosar@gmail.com>
2021-12-10 16:03:49 +0530

    Firework Svg

--- src/Particles/FireworkSvg.elm
+++ src/Particles/FireworkSvg.elm
@@ -6,5 +6,15 @@ import Utils exposing (..)
 main =
     svg
         [ viewBoxC 300 300
+        , noFill
+        , noStroke
+        , bgc black
         ]
-        []
+        [ trail vZero (vec 100 100)
+        ]
+
+
+trail s e =
+    sampleVecFromTo 50 s e
+        |> List.map (\p -> circle 10 [ xf [ mv p ] ])
+        |> group [ fill white ]
