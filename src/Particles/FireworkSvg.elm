8207ce6119787b0c9841b97768c02574a6edfafa
jigargosar <jigar.gosar@gmail.com>
2021-12-13 15:14:17 +0530

    Firework Svg

--- src/Particles/FireworkSvg.elm
+++ src/Particles/FireworkSvg.elm
@@ -106,7 +106,7 @@ initParticle nv h =
             vZero
 
         initialDisplacement =
-            vScale 0 iv
+            vScale 0.1 iv
 
         initialPosition =
             vAdd originPosition initialDisplacement
