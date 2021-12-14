1988c3e3506e3623775a033b948921c604e49c28
jigargosar <jigar.gosar@gmail.com>
2021-12-14 13:30:56 +0530

    EasingCharts.elm

--- src/EasingCharts.elm
+++ src/EasingCharts.elm
@@ -28,16 +28,48 @@ cma n =
 main =
     div [ paf 50 ]
         [ C.chart
-            []
-            [ C.xLabels []
+            [ CA.height 300
+            , CA.width 300
+            , CA.padding { top = 0, bottom = 5, left = 10, right = 10 }
+            ]
+            [ C.xLabels [ CA.withGrid ]
             , C.yLabels [ CA.withGrid ]
+            , C.bars
+                [ CA.x1 .x ]
+                [ C.bar .z [ CA.striped [] ]
+                , C.bar .y []
+                ]
+                [ { x = 3, y = 3, z = 1 }
+                , { x = 4, y = 2, z = 3 }
+                , { x = 5, y = 4, z = 2 }
+                ]
+            , C.series .x
+                [ C.stacked
+                    [ C.interpolated .y [ CA.opacity 0.2 ] []
+                    , C.interpolated .z [ CA.opacity 1, CA.dotted [] ] []
+                    ]
+                ]
+                [ { x = 1, y = 1, z = 3 }
+                , { x = 2, y = 2, z = 1 }
+                , { x = 3, y = 2, z = 4 }
+                ]
+            , C.series .x
+                [ C.scatter .y [ CA.circle ]
+                , C.scatter .z [ CA.square ]
+                ]
+                [ { x = 1, y = 2, z = 3 }
+                , { x = 2, y = 3, z = 5 }
+                , { x = 3, y = 4, z = 2 }
+                , { x = 4, y = 1, z = 3 }
+                , { x = 5, y = 4, z = 1 }
+                ]
             , C.series .x
                 [ C.interpolated .y [ CA.monotone ] [ CA.circle ]
                 , C.interpolated .z [ CA.monotone ] [ CA.square ]
                 ]
                 [ { x = 1, y = 2, z = 3 }
-                , { x = 5, y = 4, z = 1 }
-                , { x = 10, y = 2, z = 4 }
+                , { x = 3, y = 4, z = 1 }
+                , { x = 5, y = 2, z = 4 }
                 ]
             ]
         ]
