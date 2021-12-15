f0bb8a88ea63032059dd25960bdbea5b9f72c0f4
jigargosar <jigar.gosar@gmail.com>
2021-12-15 15:26:46 +0530

    SimpleFirework.elm

--- src/Particles/SimpleFirework.elm
+++ src/Particles/SimpleFirework.elm
@@ -48,26 +48,12 @@ view : Model -> Html Msg
 view model =
     let
         nl =
-            secondsToFractionOverNowMills 2 model.animNow
+            secondsToFractionOverNowMills 1 model.animNow
     in
     svg [ viewBoxC 300 300, bgc gray, dBlock, noFill, noStroke ]
-        [ easeLine (staggerNorm 0 nl) 1 vZero (vec 140 0) Ease.inSine []
-            |> to4
-        , easeLine (staggerNorm 0.1 nl) 0.1 vZero (vec 140 0) Ease.inSine [ xf [ mv2 0 10 ] ]
+        [ easeLine nl 1 vZero (vec 140 0) Ease.inSine []
             |> to4
         ]
-
-
-staggerNorm offset n =
-    let
-        nn =
-            offset + n
-    in
-    if nn > 1 then
-        nn - 1
-
-    else
-        nn
 
 
 to4 el =
