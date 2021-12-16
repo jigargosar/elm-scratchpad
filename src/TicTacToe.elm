3e626540b5090b9505d437ca6c8e9c0dbeb60678
jigargosar <jigar.gosar@gmail.com>
2021-12-16 12:14:11 +0530

    TicTacToe.elm

---
+++ src/TicTacToe.elm
@@ -0,0 +1,42 @@
+module TicTacToe exposing (main)
+
+import Utils exposing (..)
+
+
+main =
+    bElement
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = Msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Msg ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view _ =
+    div [] [ text "template" ]
