b58cdc83d86f324f2b6e3336b504b32d208f8065
jigargosar <jigar.gosar@gmail.com>
2021-12-18 15:44:45 +0530

    FireworkSMIL.elm

--- src/Particles/FireworkSMIL.elm
+++ src/Particles/FireworkSMIL.elm
@@ -13,29 +13,32 @@ main =
         [ viewBoxC 300 300
         , sMaxWidth "500px"
         ]
-        [ Svg.circle [ Px.r 10, fill <| hsl 1 1 0.5 ]
-            [ Svg.animate
-                [ SA.id "a_cx"
-                , SA.attributeName "cx"
-                , SA.values "0;100"
-                , SA.dur "2s"
-                , SA.begin "0s;a_cx.end+0.5s"
-                , SA.fill "freeze"
+        [ viewParticle ]
 
-                --, TA.repeatCount TT.RepeatIndefinite
-                ]
-                []
-            , Svg.animate
-                [ SA.id "a_op"
-                , SA.attributeName "opacity"
-                , SA.values "1;1;0"
-                , SA.keyTimes "0;0.7;1"
-                , SA.dur "2s"
-                , SA.begin "0s;a_op.end+0.5s"
-                , SA.fill "freeze"
 
-                --, TA.repeatCount TT.RepeatIndefinite
-                ]
-                []
+viewParticle =
+    Svg.circle [ Px.r 10, fill <| hsl 1 1 0.5 ]
+        [ Svg.animate
+            [ SA.id "a_cx"
+            , SA.attributeName "cx"
+            , SA.values "0;100"
+            , SA.dur "2s"
+            , SA.begin "0s;a_cx.end+0.5s"
+            , SA.fill "freeze"
+
+            --, TA.repeatCount TT.RepeatIndefinite
             ]
+            []
+        , Svg.animate
+            [ SA.id "a_op"
+            , SA.attributeName "opacity"
+            , SA.values "1;1;0"
+            , SA.keyTimes "0;0.7;1"
+            , SA.dur "2s"
+            , SA.begin "0s;a_op.end+0.5s"
+            , SA.fill "freeze"
+
+            --, TA.repeatCount TT.RepeatIndefinite
+            ]
+            []
         ]
