419b5addcef7c241e5517e8cd309039c977901fa
jigargosar <jigar.gosar@gmail.com>
2021-12-18 15:03:01 +0530

    FireworkSMIL.elm

--- src/Particles/FireworkSMIL.elm
+++ src/Particles/FireworkSMIL.elm
@@ -1,7 +1,10 @@
 module Particles.FireworkSMIL exposing (main)
 
 import Svg
+import Svg.Attributes as SA
+import TypedSvg.Attributes as TA
 import TypedSvg.Attributes.InPx as Px
+import TypedSvg.Types as TT
 import Utils exposing (..)
 
 
@@ -11,6 +14,13 @@ main =
         , sMaxWidth "500px"
         ]
         [ Svg.circle [ Px.r 10, fill <| hsl 1 1 0.5 ]
-            [ Svg.node "a" [] []
+            [ Svg.animate
+                [ SA.attributeName "cx"
+                , TA.from 0
+                , TA.to 100
+                , TA.dur <| TT.Duration "2s"
+                , TA.repeatCount TT.RepeatIndefinite
+                ]
+                []
             ]
         ]
