4db196e8db170055474fe0713bbae743058ef3fc
jigargosar <jigar.gosar@gmail.com>
2021-12-19 17:19:09 +0530

    KanBan.elm

--- src/KanBan.elm
+++ src/KanBan.elm
@@ -220,5 +220,6 @@ viewTaskItem b t =
         , style "border-radius" "10px"
         , style "border-left" ("10px solid " ++ b.color)
         , style "box-shadow" ("1px 2px 0px 1px " ++ hsla 0 0 0 1)
+        , HA.draggable "true"
         ]
         [ text t.title ]
