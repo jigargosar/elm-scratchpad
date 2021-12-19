5d4dd595a42075e971c0e4ddfa66847366c679a7
jigargosar <jigar.gosar@gmail.com>
2021-12-19 13:13:44 +0530

    KanBan.elm

--- src/KanBan.elm
+++ src/KanBan.elm
@@ -6,7 +6,7 @@ import Utils exposing (..)
 main =
     Document "Kanban"
         [ basicStylesNode
-        , div []
+        , div [ dGrid, style "grid-auto-flow" "column" ]
             [ div [] [ text "TODO" ]
             , div [] [ text "Ongoing" ]
             , div [] [ text "Done" ]
