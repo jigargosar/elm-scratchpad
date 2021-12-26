6e0e8d044b15ddd43c678db37c85bfe8afe56e10
jigargosar <jigar.gosar@gmail.com>
2021-12-26 16:12:10 +0530

    Anime v2

--- src/Anime/AnimeClone_V2.elm
+++ src/Anime/AnimeClone_V2.elm
@@ -18,52 +18,12 @@ main =
 
 type alias Model =
     { animClock : A.AnimClock
-    , particles : List Particle
-    }
-
-
-particlesForRendering : Model -> List Particle
-particlesForRendering model =
-    model.particles
-        |> List.map (updateParticleAnim model.animClock)
-
-
-initParticle : IndexLength -> Particle
-initParticle il =
-    let
-        defaultAttrs =
-            [ A.duration 1800
-            , A.loopTimes 1
-            , A.alternateDirection
-            , A.ease Ease.outElastic
-            , A.delay <| round <| (A.staggerFromCenter 200 il + 500)
-            ]
-    in
-    { x = 0
-    , xa =
-        A.anim
-            ([ defaultAttrs, [ A.fromTo 0 270 ] ] |> List.concat)
-    , a = 0
-    , aa =
-        A.anim
-            (defaultAttrs
-                ++ [ A.setTo <| A.staggerRange ( -360, 360 ) il
-                   , A.setTo 0
-                   ]
-            )
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    let
-        initialParticles : List Particle
-        initialParticles =
-            timesWithIndexAndLength 6 initParticle
-    in
-    ( { animClock = A.animClockInit
-      , particles = initialParticles
-      }
+    ( { animClock = A.animClockInit }
     , Cmd.none
     )
 
@@ -109,28 +69,7 @@ view model =
             , viewStaggerFromCenterExample model.animClock
                 |> viewExampleWithTitle "FROM VALUE"
             ]
-        , div []
-            (particlesForRendering model
-                |> List.map viewParticle
-            )
-            |> always noView
         ]
-
-
-type alias Particle =
-    { x : Float
-    , xa : A.Anim
-    , a : Float
-    , aa : A.Anim
-    }
-
-
-updateParticleAnim : A.AnimClock -> Particle -> Particle
-updateParticleAnim ac p =
-    { p
-        | x = A.valueAt p.xa ac
-        , a = A.valueAt p.aa ac
-    }
 
 
 viewStaggerFromCenterExample : A.AnimClock -> Html msg
@@ -240,52 +179,3 @@ viewLabel t aa =
             ++ aa
         )
         [ text t ]
-
-
-viewParticle : Particle -> Html msg
-viewParticle p =
-    let
-        il : IndexLength
-        il =
-            IndexLength 0 10
-    in
-    let
-        defaultAttrs =
-            [ A.duration 1800
-            , A.loopTimes 1
-            , A.alternateDirection
-            , A.ease Ease.outElastic
-            , A.delay <| round <| (A.staggerFromCenter 200 il + 500)
-            ]
-
-        _ =
-            { x =
-                A.anim
-                    ([ defaultAttrs, [ A.fromTo 0 270 ] ] |> List.concat)
-            , a =
-                A.anim
-                    (defaultAttrs
-                        ++ [ A.setTo <| A.staggerRange ( -360, 360 ) il
-
-                           --, A.setTo 0
-                           ]
-                    )
-            }
-    in
-    div
-        [ style "transform"
-            ([ "translateX(" ++ fromFloat p.x ++ "px)"
-             , "rotate(" ++ fromFloat p.a ++ "deg)"
-             ]
-                |> String.join " "
-            )
-        , bgc <| hsl 0.2 1 0.5
-        , fg black
-        , borderRadius "10px"
-        , styleWidth "50px"
-        , styleHeight "50px"
-        , ma "10px"
-        , dGrid
-        , placeContentCenter
-        ]
-        [ div [ fontSize "25px" ] [ text "A" ] ]
