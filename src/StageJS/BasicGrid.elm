4b97b7174fbaf8c1342fd906e950ee5059785112
jigargosar <jigar.gosar@gmail.com>
2021-12-28 15:31:21 +0530

    Anime v2

--- src/StageJS/BasicGrid.elm
+++ src/StageJS/BasicGrid.elm
@@ -3,6 +3,16 @@ module StageJS.BasicGrid exposing (..)
 import Utils exposing (..)
 
 
+gc =
+    { gridSize = 10, cellSize = 10 }
+
+
 main =
     basicSvg []
-        []
+        [ squareGridPositions gc.gridSize
+            |> List.map
+                (\gp ->
+                    square gc.cellSize [ fill green, xf [ mvT (gpToGridLocal gc gp) ] ]
+                )
+            |> group []
+        ]
