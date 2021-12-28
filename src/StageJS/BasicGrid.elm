41034931114edf333946831cd7c9246714e4934f
jigargosar <jigar.gosar@gmail.com>
2021-12-28 15:41:40 +0530

    Anime v2

--- src/StageJS/BasicGrid.elm
+++ src/StageJS/BasicGrid.elm
@@ -3,13 +3,26 @@ module StageJS.BasicGrid exposing (..)
 import Utils exposing (..)
 
 
+cellsInRow =
+    10
+
+
+cellSize =
+    30
+
+
+gridSize =
+    300
+
+
+gc : { gridSize : Float, cellSize : Float }
 gc =
-    { gridSize = 300, cellSize = 30 }
+    { gridSize = gridSize, cellSize = cellSize }
 
 
 main =
     basicSvg [ viewBoxC gc.gridSize gc.gridSize ]
-        [ squareGridPositions (gc.gridSize // gc.cellSize)
+        [ squareGridPositions cellsInRow
             |> List.map
                 (\gp ->
                     square gc.cellSize [ fill green, xf [ mvT (gpToGridLocal gc gp) ] ]
