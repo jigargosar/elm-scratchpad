f79964bafa2e70f1237f9be24b0d3ec9ffaf7c2b
jigargosar <jigar.gosar@gmail.com>
2022-01-02 11:51:15 +0530

    StickHeroGame.elm

--- src/StickHeroGame.elm
+++ src/StickHeroGame.elm
@@ -11,9 +11,25 @@ height =
     100
 
 
+wallHeight =
+    50
+
+
 main =
     div []
         [ basicStylesNode
         , basicSvg [ viewBoxC width height, sMaxWidth "500px" ]
-            []
+            [ [ { x = 0, w = 20 }, { x = 75, w = 20 } ]
+                |> List.map viewWall
+                |> group []
+            , viewHero
+            ]
         ]
+
+
+viewWall { x, w } =
+    rect w wallHeight [ fill white, xf [ mv2 x (wallHeight / 2) ] ]
+
+
+viewHero =
+    noView
