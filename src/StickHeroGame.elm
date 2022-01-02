01ec8e8a490e445bb37c260794eb75e507b01391
jigargosar <jigar.gosar@gmail.com>
2022-01-02 11:38:42 +0530

    StickHeroGame.elm

--- src/StickHeroGame.elm
+++ src/StickHeroGame.elm
@@ -3,9 +3,17 @@ module StickHeroGame exposing (main)
 import Utils exposing (..)
 
 
+width =
+    200
+
+
+height =
+    100
+
+
 main =
     div []
         [ basicStylesNode
-        , basicSvg []
+        , basicSvg [ viewBoxC width height, sMaxWidth "500px" ]
             []
         ]
