6ffefe6a3c788a769304640004f78027310e0c37
jigargosar <jigar.gosar@gmail.com>
2022-01-02 16:25:14 +0530

    StickHeroGame.elm

--- src/StickHeroGame.elm
+++ src/StickHeroGame.elm
@@ -40,7 +40,7 @@ step model =
                     turningAngleInDegreesSince start model
             in
             if angleDeg >= 0 then
-                { model | phase = Waiting, sticks = stick :: model.sticks }
+                { model | phase = Walking, sticks = stick :: model.sticks }
 
             else
                 model
