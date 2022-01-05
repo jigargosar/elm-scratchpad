9eca2174d8475fdd441dd200eb0b752465ee76e3
jigargosar <jigar.gosar@gmail.com>
2022-01-05 13:59:29 +0530

    StickHeroGame.elm

--- src/StickHeroGame.elm
+++ src/StickHeroGame.elm
@@ -434,20 +434,8 @@ view model =
                     , preventDefault = True
                     }
                 )
-            , Html.Events.custom "pointerdown"
-                (JD.succeed
-                    { message = OnMouseDown
-                    , stopPropagation = True
-                    , preventDefault = True
-                    }
-                )
-            , Html.Events.custom "pointerup"
-                (JD.succeed
-                    { message = OnMouseUp
-                    , stopPropagation = True
-                    , preventDefault = True
-                    }
-                )
+            , Html.Events.on "pointerdown" (JD.succeed OnMouseDown)
+            , Html.Events.on "pointerup" (JD.succeed OnMouseUp)
             ]
             [ group
                 [ xf
