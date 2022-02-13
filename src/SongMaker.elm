3d70fdc14673e8bf69e8d97b7c1d74c6a445a95d
jigargosar <jigar.gosar@gmail.com>
2022-02-13 14:55:41 +0530

    SongMaker.elm

---
+++ src/SongMaker.elm
@@ -0,0 +1,53 @@
+module BrowserDocumentTemplate exposing (main)
+
+import Utils exposing (..)
+
+
+
+{-
+   THIS LINE IS FOR FIXING INDENTATION ISSUE WITH ELM-FORMAT. DETAILS?
+    # Specs:
+
+
+-}
+
+
+main =
+    bDocument
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = NOP
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NOP ->
+            ( model, Cmd.none )
+
+
+view : Model -> Document Msg
+view _ =
+    Document "Song Maker"
+        [ basicStylesNode
+        ]
