5ca31d8287cacb7e5efd918895af0cb3ad7c7c32
jigargosar <jigar.gosar@gmail.com>
2022-02-14 13:05:01 +0530

    SongMaker.elm

---
+++ src/SongMaker.js
@@ -0,0 +1,48 @@
+import { Elm } from "./SongMaker.elm";
+import * as Tone from "tone";
+
+const synth = new Tone.PolySynth().toDestination();
+// synth.debug = true;
+// const notesSubdivisionArray = [
+//   "C4",
+//   ["E4", "D4", "E4"],
+//   "G4",
+//   ["A4", "G4"],
+// ];
+// const notes = notesSubdivisionArray.flat();
+// const noteIndices = notes.map((_, i) => i);
+// const seq = new Tone.Sequence(
+//   (time, i) => {
+//     Tone.Draw.schedule(function () {app.ports.selectColumn.send(i)}, time);
+//     synth.triggerAttackRelease(notes[i], "8n", time);
+//     // synth.triggerAttackRelease(notes[i], 0.1, time+0.05);
+//   },
+//   noteIndices,
+//   "4n"
+// );
+// seq.start(0);
+const app = Elm.SongMaker.init();
+
+app.ports.play.subscribe(async function (steps) {
+  await Tone.start();
+  Tone.Transport.stop();
+  Tone.Transport.cancel();
+  Tone.Transport.bpm.value = 120;
+
+  const seq = new Tone.Sequence(
+    (time, i) => {
+      Tone.Draw.schedule(function () {
+        app.ports.selectColumn.send(i);
+      }, time);
+      synth.triggerAttackRelease(steps[i], 0.1, time);
+    },
+    steps.map((_, i) => i),
+    "4n"
+  );
+  seq.start(0);
+  Tone.Transport.start();
+});
+app.ports.stop.subscribe(async function () {
+  await Tone.start();
+  Tone.Transport.stop();
+});
