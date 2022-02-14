b9527dd917c20fd30f7edc7aa51ffe324b783269
jigargosar <jigar.gosar@gmail.com>
2022-02-14 14:23:38 +0530

    SongMaker.elm

--- src/SongMaker.elm
+++ src/SongMaker.elm
@@ -80,7 +80,24 @@ toNotesColumns w pp =
 
         noteFromGP : Int2 -> String
         noteFromGP ( _, y ) =
-            listGetAtWithDefault "" (modBy 7 y) [ "C4", "D4", "E4", "F4", "G4", "A4", "B4" ]
+            listGetAtWithDefault
+                ""
+                y
+                [ "C4"
+                , "D4"
+                , "E4"
+                , "F4"
+                , "G4"
+                , "A4"
+                , "B4"
+                , "C5"
+                , "D5"
+                , "E5"
+                , "F5"
+                , "G5"
+                , "A5"
+                , "B5"
+                ]
 
         columnToNotesDict : Dict Int (List String)
         columnToNotesDict =
