9dda4df2ad6a73880a41d6feeab71f927e36d75d
jigargosar <jigar.gosar@gmail.com>
2022-02-18 18:02:05 +0530

    SongMaker.elm

--- src/SongMaker.elm
+++ src/SongMaker.elm
@@ -388,12 +388,11 @@ viewGrid ({ w, h, pp, cIdx, playState } as model) =
             , h100
             , positionAbsolute
             , noPointerEvents
-            , style "background-image" "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoBAMAAAB+0KVeAAAAHlBMVEUAAABkZGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRkZGSH0mEbAAAACnRSTlMAzDPDPPPYnGMw2CgMzQAAAChJREFUKM9jgAPOAgZMwGIwKkhXQSUY0BCCMxkEYUAsEM4cjI4fwYIAf2QMNbUsZjcAAAAASUVORK5CYII=')"
-            , style "background-image" "linear-gradient(to right, grey 1px, transparent 1px), linear-gradient(to bottom, grey 1px, transparent 1px)"
+            , style "background-image" "linear-gradient(to right, grey 1px, transparent 0), linear-gradient(to bottom, grey 1px, transparent 0)"
             , style "background-size"
                 (fromFloat (100 / toFloat w) ++ "% " ++ (fromFloat (100 / toFloat h) ++ "%"))
             , style "background-position" "50% 50%"
-            , style "background-position" "0 0"
+            , style "background-position" "-0.5px -0.5px"
             ]
             [ text "HH" ]
         ]
