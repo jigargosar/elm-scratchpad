0fafff36ab336a52f0c7b49b84a8b38dfce67056
jigargosar <jigar.gosar@gmail.com>
2022-02-19 16:32:11 +0530

    SongMaker.elm

--- src/SongMaker.elm
+++ src/SongMaker.elm
@@ -364,15 +364,16 @@ viewGrid ({ w, h } as model) =
             , h100
             , positionAbsolute
             , noPointerEvents
-            , backgrounds
-                [ -- major grid lines
-                  backgroundGridLinesVertical 2 (grayN 0.3) (2 / toFloat w)
-                , backgroundGridLinesHorizontal 3 (grayN 0.3) (7 / (toFloat h + 2))
+            , backgrounds <|
+                List.reverse
+                    [ -- minor grid lines
+                      backgroundGridLinesVertical 1 (grayN 0.16) (1 / toFloat w)
+                    , backgroundGridLinesHorizontal 1 (grayN 0.16) (1 / (toFloat h + 2))
 
-                -- minor grid lines
-                , backgroundGridLinesVertical 1 (grayN 0.16) (1 / toFloat w)
-                , backgroundGridLinesHorizontal 1 (grayN 0.16) (1 / (toFloat h + 2))
-                ]
+                    -- major grid lines
+                    , backgroundGridLinesVertical 2 (grayN 0.3) (2 / toFloat w)
+                    , backgroundGridLinesHorizontal 3 (grayN 0.3) (7 / (toFloat h + 2))
+                    ]
             ]
             []
         ]
