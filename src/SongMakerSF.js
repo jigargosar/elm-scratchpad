33e972d3701eefebda11e50c07fdaf4651a9abdd
jigargosar <jigar.gosar@gmail.com>
2022-02-23 14:19:45 +0530

    SongMakerSF

--- src/SongMakerSF.js
+++ src/SongMakerSF.js
@@ -1,45 +1,45 @@
-import { Elm } from "./SongMakerSF.elm";
+import { Elm } from "./SongMakerSF.elm"
 
-const app = Elm.SongMakerSF.init();
+const app = Elm.SongMakerSF.init()
 
-const Player = MakePlayer();
+const Player = MakePlayer()
 
-window.Player ??= Player;
+window.Player ??= Player
 
-app.ports.start.subscribe(Player.start);
-app.ports.stop.subscribe(Player.stop);
-app.ports.updateSteps.subscribe(Player.updateSteps);
-app.ports.playSingleNote.subscribe(Player.playSingleNote);
+app.ports.start.subscribe(Player.start)
+app.ports.stop.subscribe(Player.stop)
+app.ports.updateSteps.subscribe(Player.updateSteps)
+app.ports.playSingleNote.subscribe(Player.playSingleNote)
 
 function MakePlayer() {
-  const AudioContextFunc = window.AudioContext || window["webkitAudioContext"];
-  const audioContext = new AudioContextFunc();
+  const AudioContextFunc = window.AudioContext || window["webkitAudioContext"]
+  const audioContext = new AudioContextFunc()
 
-  const fontPlayer = new WebAudioFontPlayer();
+  const fontPlayer = new WebAudioFontPlayer()
   const presetMap = {
     synth: "_tone_" + "0000_SBLive_sf2",
     drum: "_drum_35_0_Chaos_sf2_file",
-  };
+  }
   Object.values(presetMap).forEach(function (presetVarName) {
-    fontPlayer.loader.decodeAfterLoading(audioContext, presetVarName);
-  });
+    fontPlayer.loader.decodeAfterLoading(audioContext, presetVarName)
+  })
 
-  const bpm = 120;
-  const barLengthInSeconds = (4 * 60) / bpm;
-  const totalBars = 4;
-  console.log("barLengthInSeconds: ", barLengthInSeconds);
-  const loopLengthInSeconds = totalBars * barLengthInSeconds;
-  console.log("loopLengthInSeconds:", loopLengthInSeconds);
+  const bpm = 120
+  const barLengthInSeconds = (4 * 60) / bpm
+  const totalBars = 4
+  console.log("barLengthInSeconds: ", barLengthInSeconds)
+  const loopLengthInSeconds = totalBars * barLengthInSeconds
+  console.log("loopLengthInSeconds:", loopLengthInSeconds)
   // const noteDuration = (1 / 16) * barLengthInSeconds;
-  const noteGap = (1 / 8) * barLengthInSeconds;
-  const noteDuration = noteGap;
+  const noteGap = (1 / 8) * barLengthInSeconds
+  const noteDuration = noteGap
 
-  const ticker = new WebAudioFontTicker();
+  const ticker = new WebAudioFontTicker()
 
-  let steps = null;
+  let steps = null
 
   function playNote([presetName, note], at = 0) {
-    const presetVar = window[presetMap[presetName]];
+    const presetVar = window[presetMap[presetName]]
     fontPlayer.queueWaveTable(
       audioContext,
       audioContext.destination,
@@ -49,7 +49,7 @@ function MakePlayer() {
       noteDuration,
       0.5,
       []
-    );
+    )
   }
 
   function playLoop(
@@ -60,20 +60,20 @@ function MakePlayer() {
     loopEnd,
     steps_
   ) {
-    steps = steps_;
+    steps = steps_
     ticker.startTicks(
       audioContext,
       function (wallClock, from, to) {
         for (let i = 0; i < steps.length; i++) {
-          const noteStartTimeInLoop = i * noteGap;
+          const noteStartTimeInLoop = i * noteGap
           if (noteStartTimeInLoop >= from && noteStartTimeInLoop < to) {
-            const scheduleDelay = noteStartTimeInLoop - from;
+            const scheduleDelay = noteStartTimeInLoop - from
             setTimeout(
               () => app.ports.selectColumn.send(i),
               scheduleDelay - 0.1
-            );
-            const scheduleTime = wallClock + scheduleDelay;
-            steps[i].forEach((data) => playNote(data, scheduleTime));
+            )
+            const scheduleTime = wallClock + scheduleDelay
+            steps[i].forEach((data) => playNote(data, scheduleTime))
           }
         }
       },
@@ -81,17 +81,17 @@ function MakePlayer() {
       loopPosition,
       loopEnd,
       function () {
-        player.cancelQueue(audioContext);
+        player.cancelQueue(audioContext)
       }
-    );
+    )
   }
 
   return {
     updateSteps(steps_) {
-      steps = steps_;
+      steps = steps_
     },
     start(steps_) {
-      ticker.cancel();
+      ticker.cancel()
       playLoop(
         fontPlayer,
         audioContext,
@@ -99,13 +99,13 @@ function MakePlayer() {
         ticker.lastPosition, // 0,
         loopLengthInSeconds,
         steps_
-      );
+      )
     },
     stop() {
-      ticker.cancel();
+      ticker.cancel()
     },
     playSingleNote(data) {
-      playNote(data);
+      playNote(data)
     },
-  };
+  }
 }
