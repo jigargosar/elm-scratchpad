6505d50aaa151ae68925f6636ceef2ae4ee319a1
jigargosar <jigar.gosar@gmail.com>
2022-02-26 15:41:36 +0530

    SongMakerSF

--- src/SongMakerSF.js
+++ src/SongMakerSF.js
@@ -20,10 +20,10 @@ function MakePlayer() {
 
   const fontPlayer = new WebAudioFontPlayer()
   const presetMap = {
-    piano: "_tone_" + "0000_SBLive_sf2",
-    // bassDrum2: "_drum_35_0_Chaos_sf2_file",
+    piano: "_tone_0000_SBLive_sf2", // bassDrum2: "_drum_35_0_Chaos_sf2_file",
     bassDrum1: "_drum_36_0_Chaos_sf2_file",
     snareDrum2: "_drum_40_0_Chaos_sf2_file",
+    strings: "_tone_0450_Chaos_sf2_file",
   }
   const presetNames = Object.values(presetMap)
   loadPresets(audioContext, fontPlayer, presetNames)
