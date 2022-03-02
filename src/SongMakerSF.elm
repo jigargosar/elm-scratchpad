972335ae23ccd269221504ad96e354b35f44f1e3
jigargosar <jigar.gosar@gmail.com>
2022-03-02 17:07:03 +0530

    SongMakerSF: trying to split grid view

--- src/SongMakerSF.elm
+++ src/SongMakerSF.elm
@@ -974,38 +974,15 @@ viewGrid2 model =
               in
               div [ dGrid, styleGridTemplate w h ]
                 (rangeWH w h |> List.map (viewPercussionTileAt model))
-            , let
-                s =
-                    model.settings
-
-                w =
-                    toFloat (computeGridWidth s)
-              in
-              div
-                [ w100
-                , h100
-                , positionAbsolute
-                , noPointerEvents
-                , backgrounds
-                    (List.reverse
-                        [ -- minor grid lines
-                          backgroundGridLinesVertical 1 (grayN 0.16) (1 / w)
-
-                        --, backgroundGridLinesHorizontal 1 (grayN 0.16) (1 / h)
-                        -- major grid lines
-                        , backgroundGridLinesVertical 2 (grayN 0.3) (toFloat s.beatSplits / w)
-
-                        --, backgroundGridLinesHorizontal 3
-                        --    (grayN 0.3)
-                        --    (toFloat (musicScaleLength s.scale) / h)
-                        ]
-                    )
-                ]
-                []
+            , viewPercussionGridLines
+                { w = computeGridWidth model.settings
+                , beatSplits = model.settings.beatSplits
+                }
             ]
         ]
 
 
+viewPercussionGridLines : { a | w : Int, beatSplits : Int } -> Html msg
 viewPercussionGridLines { w, beatSplits } =
     div
         [ w100
