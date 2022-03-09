03f9fa2b68e397e685f7de7d048ffc929ecc46a8
jigargosar <jigar.gosar@gmail.com>
2022-03-09 14:19:22 +0530

    SongMakerSF: refactor noteName computation

--- src/SongMakerSF.elm
+++ src/SongMakerSF.elm
@@ -635,37 +635,6 @@ instrumentNoteFromGP audioTime model ( _, y ) =
         pitch =
             noteName ++ fromInt noteOctaveNum
 
-        _ =
-            if pitch /= pitch1 then
-                let
-                    _ =
-                        Debug.log "Debug: " ( ( y, octaveOffset ), pitch1, pitch )
-                in
-                Debug.todo "ERROR: ^^^"
-
-            else
-                1
-
-        noteNames =
-            [ "C3"
-            , "D3"
-            , "E3"
-            , "F3"
-            , "G3"
-            , "A3"
-            , "B3"
-            , "C4"
-            , "D4"
-            , "E4"
-            , "F4"
-            , "G4"
-            , "A4"
-            , "B4"
-            ]
-
-        pitch1 =
-            listGetAtOrDefault "" y noteNames
-
         presetName =
             case model.instrument of
                 Piano ->
