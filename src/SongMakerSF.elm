ee5ff36bd5305d7e73840eaa627ed4f6dabd2fa8
jigargosar <jigar.gosar@gmail.com>
2022-03-23 16:40:25 +0530

    SongMakerSF: bottom bar visuals

--- src/SongMakerSF.elm
+++ src/SongMakerSF.elm
@@ -1804,7 +1804,7 @@ viewBottomBar model =
         , iconButton SettingsClicked
             "Settings"
             [ HA.id "settings-btn" ]
-            settingsIconSvg
+            settingsIcon
         , iconButton UndoClicked
             ("Undo " ++ fromInt (model.dataModelPivot |> Pivot.lengthL))
             []
@@ -2200,6 +2200,10 @@ iconSize =
     60
 
 
+innerIconSize =
+    32
+
+
 pianoIconSvg : Html msg
 pianoIconSvg =
     svg
@@ -2244,25 +2248,17 @@ materialIconColor =
     Material.Icons.Types.Color iconColor_
 
 
-settingsIconSvg : Svg msg
-settingsIconSvg =
-    svg
-        [ viewBoxLT 24 24
-        , sWidth iconSizePxString
+settingsIcon : Html msg
+settingsIcon =
+    div
+        [ styleWidth iconSizePxString
+        , styleHeight iconSizePxString
         , borderRadius "50%"
         , style "border" ("1px solid " ++ grayN 0.3)
-        , fill iconColor
+        , displayGrid
+        , placeContentCenter
         ]
-        [ Svg.path [ SA.d settingsIconSvgPath ] []
-        ]
-
-
-
---|> always (Material.Icons.settings iconSize materialIconColor)
-
-
-settingsIconSvgPath =
-    "M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z"
+        [ Material.Icons.settings innerIconSize materialIconColor ]
 
 
 undoIconSvg : Svg msg
