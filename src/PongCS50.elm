0773c3ecf20c2e92ec7c2755b42ab62ed46542ab
jigargosar <jigar.gosar@gmail.com>
2022-05-25 12:38:11 +0530

    PongCS50.elm: init

---
+++ src/PongCS50.elm
@@ -0,0 +1,53 @@
+module PongCS50 exposing (main)
+
+import Utils exposing (..)
+
+
+
+{-
+   THIS LINE IS FOR FIXING INDENTATION ISSUE WITH ELM-FORMAT. DETAILS?
+    # Specs:
+
+
+-}
+
+
+main =
+    bDocument
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = viewDocument
+        }
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = NOP
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NOP ->
+            ( model, Cmd.none )
+
+
+viewDocument : Model -> Document Msg
+viewDocument _ =
+    Document "PongCS50"
+        [ basicStylesNode
+        ]
