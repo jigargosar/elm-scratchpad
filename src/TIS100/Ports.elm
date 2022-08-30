0520c58cdc8d60a3ae058bd7bb3a7473375ad466
jigargosar <jigar.gosar@gmail.com>
2022-08-30 13:24:18 +0530

    TIS100.elm: implementing basic simulation

--- src/TIS100/Ports.elm
+++ src/TIS100/Ports.elm
@@ -1,7 +1,10 @@
 module TIS100.Ports exposing (..)
 
 import Dict exposing (Dict)
+import TIS100.IOIntent exposing (IOIntent)
+import TIS100.NodeState exposing (NodeState)
 import TIS100.Num exposing (Num)
+import TIS100.Puzzle exposing (Puzzle)
 
 
 type Ports
@@ -24,3 +27,18 @@ type PortValue
     = Empty
     | Num Num
     | Queried
+
+
+init : Ports
+init =
+    Debug.todo "todo"
+
+
+fromPuzzle : Puzzle -> Ports
+fromPuzzle puzzle =
+    Debug.todo "todo"
+
+
+fromIOIntentsAndNodeState : Puzzle -> List ( IOIntent, NodeState a ) -> Ports
+fromIOIntentsAndNodeState puzzle list =
+    Debug.todo "todo"
