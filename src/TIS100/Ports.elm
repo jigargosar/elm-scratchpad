8a65fe10587ade2788ac1120481da1ab7788c2f6
jigargosar <jigar.gosar@gmail.com>
2022-08-30 14:44:09 +0530

    TIS100.elm: implementing basic simulation

--- src/TIS100/Ports.elm
+++ src/TIS100/Ports.elm
@@ -1,11 +1,12 @@
 module TIS100.Ports exposing (fromPuzzle)
 
 import Dict exposing (Dict)
+import Html exposing (Html)
 import TIS100.IOIntent exposing (IOIntent(..))
 import TIS100.NodeState exposing (NodeState)
-import TIS100.Num exposing (Num)
+import TIS100.Num as Num exposing (Num)
 import TIS100.Puzzle as Puzzle exposing (Puzzle)
-import Utils exposing (Dir4(..), moveInDir4, oppositeDir4)
+import Utils exposing (..)
 
 
 type alias Ports =
@@ -112,6 +113,88 @@ fromPuzzle puzzle =
     ids |> List.map (\( _, addr, dir ) -> ( addr, dir, Empty ))
 
 
+viewPort : ( Addr, Dir4, PortValue ) -> Html msg
+viewPort ( addr, dir, portValue ) =
+    case dir of
+        Up ->
+            viewUpPortValue addr portValue
+
+        Down ->
+            viewDownPortValue addr portValue
+
+        Left ->
+            noView
+
+        Right ->
+            noView
+
+
+viewDownPortValue : Addr -> PortValue -> Html msg
+viewDownPortValue ( x, y ) portValue =
+    gtCols 2
+        [ gridAreaXY ( x * 2, y * 2 ), noPointerEvents ]
+        [ fRow
+            [ gridAreaXY ( 1, 0 )
+            , allPointerEvents
+            , itemsCenter
+            , pl "1ch"
+            , gap "1ch"
+            ]
+            [ viewArrow Down portValue
+            , viewPortValueText portValue
+            ]
+        ]
+
+
+viewUpPortValue : Addr -> PortValue -> Html msg
+viewUpPortValue ( x, y ) portValue =
+    gtCols 2
+        [ gridAreaXY ( x * 2, (y * 2) - 2 ), noPointerEvents ]
+        [ fRow
+            [ gridAreaXY ( 0, 0 )
+            , allPointerEvents
+            , itemsCenter
+            , justifyContent "end"
+            , pr "1ch"
+            , gap "1ch"
+            ]
+            [ viewPortValueText portValue
+            , viewArrow Up portValue
+            ]
+        ]
+
+
+viewPortValueText : PortValue -> Html msg
+viewPortValueText =
+    let
+        toString portValue =
+            case portValue of
+                Empty ->
+                    ""
+
+                Num num ->
+                    Num.toString num
+
+                Queried ->
+                    "?"
+    in
+    toString >> text
+
+
+viewArrow : Dir4 -> PortValue -> Html msg
+viewArrow dir4 pv =
+    let
+        color =
+            case pv of
+                Empty ->
+                    darkGray
+
+                _ ->
+                    "inherit"
+    in
+    span [ fg color, fontSize "2em", fontWeight "100" ] [ text (arrowDefault dir4) ]
+
+
 
 --noinspection ElmUnusedSymbol
 
@@ -119,3 +202,69 @@ fromPuzzle puzzle =
 fromIOIntentsAndNodeState : Puzzle -> List ( IOIntent, NodeState a ) -> Ports
 fromIOIntentsAndNodeState _ _ =
     Debug.todo "todo"
+
+
+
+-- UI HELPERS
+
+
+darkGray =
+    grayN 0.5
+
+
+lightGray =
+    grayN 0.7
+
+
+lightOutline =
+    sOutline ("1px solid " ++ lightGray)
+
+
+type ArrowType
+    = Filled
+    | Outline
+
+
+defaultArrowType =
+    Filled
+
+
+arrowDefault =
+    case defaultArrowType of
+        Filled ->
+            arrowFilled
+
+        Outline ->
+            arrowOutline
+
+
+arrowOutline : Dir4 -> String
+arrowOutline dir4 =
+    case dir4 of
+        Up ->
+            "⇧"
+
+        Down ->
+            "⇩"
+
+        Left ->
+            "⇦ "
+
+        Right ->
+            "⇨"
+
+
+arrowFilled : Dir4 -> String
+arrowFilled dir4 =
+    case dir4 of
+        Up ->
+            "⬆"
+
+        Down ->
+            "⬇"
+
+        Left ->
+            "⬅"
+
+        Right ->
+            "➡"
