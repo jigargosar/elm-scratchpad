e0635c69609c1880d8dba6a38c6b9260ddbc5848
jigargosar <jigar.gosar@gmail.com>
2022-09-01 18:46:49 +0530

    TIS100.elm: implementing basic simulation

--- src/TIS100/Sim2.elm
+++ src/TIS100/Sim2.elm
@@ -8,7 +8,7 @@ module TIS100.Sim2 exposing
 
 import Dict exposing (Dict)
 import TIS100.ExeNode as ExeNode exposing (ExeNode)
-import TIS100.Grid as Grid exposing (Node(..))
+import TIS100.Grid as Grid exposing (Addr, Node(..))
 import TIS100.InputNode as InputNode exposing (InputNode)
 import TIS100.NodeState as S exposing (NodeState)
 import TIS100.Num as Num exposing (Num)
@@ -418,14 +418,6 @@ viewOutputColumn { title, expected, actual } =
                 )
             ]
         ]
-
-
-
--- ADDR
-
-
-type alias Addr =
-    ( Int, Int )
 
 
 
