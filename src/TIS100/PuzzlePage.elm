e3202f407b595681f4224d407ffb87d3d8c40dab
jigargosar <jigar.gosar@gmail.com>
2022-09-04 16:24:34 +0530

    TIS100.elm: compile

--- src/TIS100/PuzzlePage.elm
+++ src/TIS100/PuzzlePage.elm
@@ -29,21 +29,8 @@ import Utils exposing (..)
 sampleModel : Model
 sampleModel =
     let
-        --exs =
-        --    [ ( ( 0, 1 ), ExeNode.initMov Up Down )
-        --    , ( ( 0, 2 ), ExeNode.initMovUpDown )
-        --    , ( ( 0, 3 ), ExeNode.initMovUpDown )
-        --    , ( ( 1, 1 ), ExeNode.initMov Right Down )
-        --    , ( ( 1, 2 ), ExeNode.empty )
-        --    , ( ( 1, 3 ), ExeNode.initMovUpDown )
-        --    , ( ( 2, 1 ), ExeNode.initMov Down Up )
-        --    , ( ( 2, 2 ), ExeNode.empty )
-        --    , ( ( 2, 3 ), ExeNode.initMov Down Up )
-        --    , ( ( 3, 1 ), ExeNode.initMov Left Down )
-        --    , ( ( 3, 2 ), ExeNode.initMov Up Right )
-        --    , ( ( 3, 3 ), ExeNode.initMov Left Down )
-        --    ]
-        es =
+        ss : List ( Addr, String )
+        ss =
             [ ( ( 0, 1 ), "Mov up down" )
             , ( ( 0, 2 ), "Mov up down" )
             , ( ( 0, 3 ), "Mov up down" )
@@ -58,7 +45,7 @@ sampleModel =
             , ( ( 3, 3 ), "Mov up down" )
             ]
     in
-    init Puzzle.samplePuzzle es
+    init Puzzle.samplePuzzle ss
 
 
 
