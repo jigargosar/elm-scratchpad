e0cf743eb728a96ee3a1fe1a21e6fe96f4a695bd
jigargosar <jigar.gosar@gmail.com>
2022-09-13 15:05:55 +0530

    TIS100.elm: trying to use simple text parsing for compiling

--- tests/Compiler2Test.elm
+++ tests/Compiler2Test.elm
@@ -1,7 +1,9 @@
 module Compiler2Test exposing (..)
 
 import Expect
+import List.Extra
 import Test exposing (Test, describe, test)
+import Utils as U
 
 
 
@@ -23,7 +25,18 @@ type alias Error =
 
 compile : String -> Result Error ()
 compile s =
-    Err (Error InvalidOp s 0)
+    let
+        col =
+            s
+                |> String.toList
+                |> List.indexedMap Tuple.pair
+                |> List.Extra.dropWhile (U.second >> U.eq ' ')
+                |> List.head
+                |> Maybe.map U.first
+                |> Maybe.withDefault 0
+                |> Debug.log "Debug: "
+    in
+    Err (Error InvalidOp s col)
 
 
 suite : Test
@@ -31,8 +44,8 @@ suite =
     test "invalid op" <|
         \_ ->
             expectErrorOnCompile
-                ( " foo"
-                , "-^"
+                ( "  foo"
+                , "--^"
                 , InvalidOp
                 )
 
@@ -41,7 +54,7 @@ expectErrorOnCompile : ( String, String, Prob ) -> Expect.Expectation
 expectErrorOnCompile ( src, marker, prob ) =
     case compile src of
         Err err ->
-            ( src, String.repeat (err.col - 1) "-" ++ "^", err.msg )
+            ( src, String.repeat err.col "-" ++ "^", err.msg )
                 |> Expect.equal ( src, marker, prob )
 
         Ok _ ->
