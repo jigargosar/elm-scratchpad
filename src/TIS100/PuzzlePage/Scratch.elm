adfeef2918bf09b1af11f087213b283a3c79de4a
jigargosar <jigar.gosar@gmail.com>
2022-09-15 15:47:54 +0530

    TIS100.elm: trying to render error text decoration

---
+++ src/TIS100/PuzzlePage/Scratch.elm
@@ -0,0 +1,50 @@
+module TIS100.PuzzlePage.Scratch exposing (..)
+
+import Html exposing (input, textarea)
+import TIS100.UI as UI
+import Utils exposing (..)
+
+
+main : Html.Html msg
+main =
+    div []
+        [ basicStylesNode
+        , fCol
+            [ h100
+            , fontSize "12px"
+            , styleLineHeight "0.9"
+            , pa "3ch"
+            , bold
+            , ffMonospace
+            , gap "2ch"
+            , ttu
+            ]
+            [ div
+                [ displayGrid
+                , sWidth "18ch"
+                , sHeight UI.nodeSize
+                ]
+                [ viewEditor ]
+            ]
+        ]
+
+
+viewEditor =
+    Html.textarea
+        [ -- reset
+          borderNone
+        , outlineNone
+        , resizeNone
+
+        -- inherit
+        , bgcInherit
+        , fgInherit
+        , ttInherit
+        , fontInherit
+
+        --, onInput onInputMsg
+        -- actual
+        , sOutline "1px solid white"
+        , pa "0.5ch"
+        ]
+        [ text "editor" ]
