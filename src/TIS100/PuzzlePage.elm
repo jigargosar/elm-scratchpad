abe829202117a8952d9475bc7d836c3b24ad17d7
jigargosar <jigar.gosar@gmail.com>
2022-09-18 16:04:37 +0530

    TIS100.elm: refactoring view editors

--- src/TIS100/PuzzlePage.elm
+++ src/TIS100/PuzzlePage.elm
@@ -286,8 +286,8 @@ viewOutputNode { x, title } =
         ]
 
 
-viewEditor : ( Addr, Editor ) -> Html Msg
-viewEditor ( addr, editor ) =
+viewEditor : Addr -> Editor -> Html Msg
+viewEditor addr editor =
     let
         errors =
             Compiler.getErrorDetails editor
@@ -482,12 +482,11 @@ viewEditModeNodes puzzle editors =
     Puzzle.toDictBy
         { in_ = viewInputNode
         , out = viewOutputNode
-        , exe =
-            \addr ->
-                maybeView viewEditor (getEntry addr editors)
+        , exe = always noView
         , flt = viewFaultyNode
         }
         puzzle
+        |> Dict.union (Dict.map viewEditor editors)
         |> Dict.values
 
 
